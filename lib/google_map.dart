112f0d90806dd34a7b804ac91d66ad5dabc08dd2
codershakibs001 <shakib501886@gmail.com>
2025-05-02 17:50:37 +0600

    data added

---
+++ lib/google_map.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class GoogleMapScreen extends StatefulWidget {
+  const GoogleMapScreen({super.key});
+
+  @override
+  State<GoogleMapScreen> createState() => _GoogleMapScreenState();
+}
+
+class _GoogleMapScreenState extends State<GoogleMapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
